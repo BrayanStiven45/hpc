09be79f71170493992f06c0e01fd0faa8efc2dda
BrayanStiven45 <bravalen456@gmail.com>
2026-04-25 22:43:30 -0500

    una pequeña modificacion al test.sh para que tambien tome datos de tiempo para el sequencial

--- openmp/test.sh
+++ openmp/test.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
 sizes=(400 600 800 1200 1600 2400 3200 4800 6400 9600)
-OUTPUT="times.csv"
+OUTPUT_OPENMP="times_openmp.csv"
+OUTPUT_SEQUENTIAL="times_sequential.csv"
 
-# Write header row
-echo -n "Run" > $OUTPUT
+mkdir -p bin
+
+gcc -fopenmp -o bin/openmp openmp.c
+gcc -o bin/sequential sequential.c
+
+echo -n "Run" > $OUTPUT_OPENMP
+for s in "${sizes[@]}"; do echo -n ",$s" >> $OUTPUT_OPENMP; done
+echo "" >> $OUTPUT_OPENMP
+
+for j in {1..10}; do
+echo -n "$j" >> $OUTPUT_OPENMP
 for s in "${sizes[@]}"; do
-  echo -n ",$s" >> $OUTPUT
+time_openmp=$(./bin/openmp $s)
+echo -n ",$time_openmp" >> $OUTPUT_OPENMP
+done
+echo "" >> $OUTPUT_OPENMP
 done
 
-echo "" >> $OUTPUT
-  
-# Runs
+echo -n "Run" > $OUTPUT_SEQUENTIAL
+for s in "${sizes[@]}"; do echo -n ",$s" >> $OUTPUT_SEQUENTIAL; done
+echo "" >> $OUTPUT_SEQUENTIAL
+
 for j in {1..10}; do
-  echo -n "$j" >> $OUTPUT
-  
-  echo "Test $j with each size:"
-  for s in "${sizes[@]}"; do
-    time=$(./bin/main $s)
-    echo -n ",$time" >> $OUTPUT
-    echo "$s x $s done!"
-  done
-    
-  echo "" >> $OUTPUT
-  echo ""
+echo -n "$j" >> $OUTPUT_SEQUENTIAL
+for s in "${sizes[@]}"; do
+time_sequential=$(./bin/sequential $s)
+echo -n ",$time_sequential" >> $OUTPUT_SEQUENTIAL
+done
+echo "" >> $OUTPUT_SEQUENTIAL
 done
